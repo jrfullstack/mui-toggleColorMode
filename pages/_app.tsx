20e580e7ae6d67e5b0756ce25dd5018309bb3815
Jimmy Reyes <jrfullstack@gmail.com>
2023-04-19 18:53:10 +0200

    instalacion y limpiez

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,78 @@
-import '@/styles/globals.css'
+import * as React from "react";
 import type { AppProps } from 'next/app'
+import '@/styles/globals.css'
+import Box from "@mui/material/Box";
+import { useTheme, ThemeProvider, createTheme, IconButton, Divider } from "@mui/material";
+import Brightness4Icon from "@mui/icons-material/Brightness4";
+import Brightness7Icon from "@mui/icons-material/Brightness7";
+
+const ColorModeContext = React.createContext({ toggleColorMode: () => {} });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  const theme = useTheme();
+
+  const colorMode = React.useContext(ColorModeContext);
+
+  const ToggleColorMode = () => {
+    const [mode, setMode] = React.useState<"light" | "dark">("light");
+    const colorMode = React.useMemo(
+      () => ({
+          toggleColorMode: () => {
+              setMode((prevMode) =>
+                  prevMode === "light" ? "dark" : "light"
+              );
+          },
+      }),
+      []
+    );
+
+    const theme = React.useMemo(
+      () =>
+          createTheme({
+              palette: {
+                  mode,
+              },
+          }),
+      [mode]
+    );
+  };
+  return (
+      <>
+          <Box
+              sx={{
+                  display: "flex",
+                  width: "100%",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  bgcolor: "background.default",
+                  color: "text.primary",
+                  borderRadius: 1,
+                  p: 3,
+              }}>
+              {/* {theme.palette.mode} mode */}
+              <IconButton
+                  sx={{ ml: 1 }}
+                  onClick={colorMode.toggleColorMode}
+                  color="inherit">
+                  {theme.palette.mode === "dark" ? (
+                      <Brightness7Icon />
+                  ) : (
+                      <Brightness4Icon />
+                  )}
+              </IconButton>
+          </Box>
+          <Box>
+              <ColorModeContext.Provider value={colorMode}>
+                  <ThemeProvider theme={theme}>
+                      {/* <App /> */}
+                      <Component {...pageProps} />
+                  </ThemeProvider>
+              </ColorModeContext.Provider>
+          </Box>
+      </>
+  );
+  
+    
 }
+
